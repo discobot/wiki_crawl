e1bd52bfe7cbab5b272e403844d4b40df91d6cb2
Philip <ph.sinitsyn@gmail.com>
2014-07-31 20:44:52 +0400

    line break in config.py

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ article_pattern = domain + "/wiki/([\w\(\)]+)"
 article_crawlers = 10
 utility_crawlers = 5
 
-wiki_utility_pages = ["/wiki/" + type for type in ["Talk:", "Category:", "Help:", "Special:", "Wikipedia:", "Wikipedia_talk:", "Template:", "T:", "User:", "User_talk", "User_talk"]]
+wiki_utility_pages = ["/wiki/" + type for type in ["Talk:", "Category:", "Help:", "Special:", 
+                                     "Wikipedia:", "Wikipedia_talk:", "Template:", "T:", "User:", "User_talk", "User_talk"]]
 wiki_ignored_pages = ["/wiki/File:", "index.php", "query.php", "api.php", "pagename"]
 
 ban_prefix = ["mailto"]
